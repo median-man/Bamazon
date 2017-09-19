e6c010c5247be66a11ef4d237fcb791e6181a252
John Desrosiers <johndrunner@yahoo.com>
2017-09-19 16:56:43 -0700

    bug fix. handle undefined id argument

--- BamazonDB.js
+++ BamazonDB.js
@@ -37,14 +37,12 @@ var BamazonDB = (function(username, password) {
     // Returns a promise. Passes query result to callback.
     // optional id paremeter looks up product by id
     var queryProducts = function(id) {
-        // sql command
         var sql = "SELECT * FROM " + tblProducts;
-        // escape sql key words
-        id = connection.escape(id);
-
+        
         // if an id argument was provided, update sql
         // with a WHERE clause
         if ( typeof id !== 'undefined' ) {
+            id = connection.escape(id); // escape sql key words
             sql += " WHERE " + colProductID + " = " + id;
         }
         
@@ -52,13 +50,11 @@ var BamazonDB = (function(username, password) {
         // to the callback on succesful query
         return new Promise(
             function(resolve, reject) {
-                // query the database and pass values to
-                // promise callbacks
+
+                // query the products table
                 connection.query(sql, 
                     function(err, results) {
-                        // pass err to error callback
                         if (err) return reject(err);
-                        // pass query results to callback
                         resolve(results);
                     }
                 );

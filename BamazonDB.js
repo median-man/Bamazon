abc2091486492fe834fb25a0eea8e124a81e5e0f
John Desrosiers <johndrunner@yahoo.com>
2017-09-18 18:05:20 -0700

    added queryProducts method

--- BamazonDB.js
+++ BamazonDB.js
@@ -6,9 +6,9 @@
  * Import modules
  * */
 const mysql = require("mysql");
-const keys = require("./keys").bamazon_db;
+const keys = require("./keys.json").bamazon_db;
 
-// configuration settings for the database
+// configuration settings for connecting to the database
 const dbConfig = {
     user: keys.username,
     password: keys.password,
@@ -16,6 +16,9 @@ const dbConfig = {
     port: 3306,
     database: "bamazon_DB",
 };
+
+// database constants
+const tblProducts = "products";
 
 // Create a class to provide an interface with the database
 // and immediately get an instance of the class
@@ -29,10 +32,34 @@ var BamazonDB = (function(username, password) {
         connection.end();
     };
 
+    // Returns a promise. Passes query result to callback.
+    var queryProducts = function() {
+        // sql command
+        var sql = "SELECT * FROM " + tblProducts;
+        return new Promise(
+            function(resolve, reject) {
+                // query the data base and pass values to
+                // promise callbacks
+                connection.query(sql, 
+                    function(err, results) {
+                        // pass err to error callback
+                        if (err) return reject(err);
+                        // pass query results to callback
+                        resolve(results);
+                    }
+                );
+            }
+        );
+    };
+
     return {
-        close: close
+        close: close,
+        queryProducts: queryProducts
     };
 })();
 
 // test code
+BamazonDB.queryProducts().then(console.log);
 BamazonDB.close();
+
+module.exports = BamazonDB;

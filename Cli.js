a0816f0e461bbcca57a0d256736752e6b37cb5bc
John Desrosiers <johndrunner@yahoo.com>
2017-09-20 05:17:57 -0700

    backup from modular approach

--- Cli.js
+++ Cli.js
@@ -9,6 +9,67 @@ const inquirer = require("inquirer");
 
 var Cli = function() {
 
+    // column widths for listing products table data
+    const colId     = 4,
+        colItem     = 50,
+        colDept     = 10,
+        colPrice    = 7,
+        colQty      = 5;
+
+    // renders a list of products with information
+    // on the command line
+    function listProducts(products) {
+
+        // column headings formatted
+        var header = [
+            getRow({
+                item_id         : "Id",
+                product_name    : "Product",
+                department_name : "Dept.",
+                price           : "Price",
+                stock_quantity  : "Quantity"
+            }),
+            getRow({
+                item_id         : "-".repeat(colId),
+                product_name    : "-".repeat(colItem),
+                department_name : "-".repeat(colDept),
+                price           : "-".repeat(colPrice),
+                stock_quantity  : "-".repeat(colQty)
+            })
+        ];
+
+        // get strings for each row from items
+        var rows = products.map(function(product) {
+            return getRow(product);
+        });
+
+        // display the table
+        rows = header.concat(rows);
+        console.log(rows.join("\n"));
+    }
+
+    // returns a string for a row in the products list
+    // spaced to display data in tabular form
+    function getRow(values) {
+        // column widths (+1 for padding between columns)
+        var row =  fixedWidth(values.item_id.toString(), colId) + " " +
+            fixedWidth(values.product_name, colItem) + " " +
+            fixedWidth(values.department_name, colDept) + " " +
+            fixedWidth(values.price.toString(), colPrice) + " " +
+            fixedWidth(values.stock_quantity.toString(), colQty);
+        return row;
+    }
+
+    // returns string truncated or with padding added to fix
+    // the width of the string. pad_char must be a single character.
+    function fixedWidth(s, length, pad_char = " ") {
+        if ( s.length < length ) {
+            return s + pad_char.repeat(length - s.length);
+        } else {
+            return s.substr(0,length);
+        }
+    }
+
     // Prompts user to continue shopping
     this.continueShopping = function() {
         console.log("continue shopping?");
@@ -29,11 +90,37 @@ var Cli = function() {
 
     // Returns item id and quantity desired from
     // user input
-    this.newPurchase = function() {
-        return {
-            id: 5,
-            quantity: 1
-        };
+    this.newPurchase = function(products) {
+        var message = "Enter the ID for the item you would like to " +
+            "purchase or 'Q' to quit:";
+        var result = {id:"", quantity:0};
+
+        // display a table of products
+        listProducts(products);
+
+        // prompt user and return promise passing
+        // user input to callback
+        return inquirer.prompt([
+            {
+                message: message,
+                type: "input",
+                name: "id"
+            },
+            {
+                message: "How many?",
+                type: "input",
+                name: "quantity",
+                when: function(answers) {
+                    return answers !== "Q";
+                }
+            }
+            
+        // return the user input
+        ]).then(function(answers) {
+            result.id = answers.id;
+            if ( answers.quantity ) result.quantity = answers.quantity;
+            return result;
+        });
     };
 };
 

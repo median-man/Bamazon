8c8cf8b1e035093868e7f6ecba9051cd66feece5
John Desrosiers <johndrunner@yahoo.com>
2018-01-07 10:06:31 -0800

    added showAllproducts method

--- src/bamazonManager.js
+++ src/bamazonManager.js
@@ -1,12 +1,26 @@
 const managerView = require('./managerView.js');
+const customerView = require('./customerView.js');
+const BamazonDB = require('./BamazonDB.js');
 
+const db = new BamazonDB();
+function showAllProducts() {
+  return db
+    .getTable()
+    .then(customerView.renderProducts);
+}
+function quit() {
+  db.connection.end();
+}
 function run() {
   managerView
     .mainMenu()
     .then(({ action }) => {
-      if (action !== 'quit') {
-        run();
+      if (action === 'all') {
+        return showAllProducts()
+          .then(run);
       }
+      if (action === 'quit') return quit();
+      return run();
     })
     .catch((err) => { throw err; });
 }
